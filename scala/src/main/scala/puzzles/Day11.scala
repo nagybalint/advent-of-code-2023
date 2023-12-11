a38ac0da24d653af61834d30857d06c18c141e29
Balint Nagy <bnagy@cloudera.com>
2023-12-11 11:02:35 +0100

    Remove unused method

--- scala/src/main/scala/puzzles/Day11.scala
+++ scala/src/main/scala/puzzles/Day11.scala
@@ -9,19 +9,15 @@ object Day11 {
   type GalaxyPair = (Coordinate, Coordinate)
 
   def parseImage(in: Seq[String]): Image = in.map(_.toCharArray.toList).toList
-  def findGalaxies(image: Image): List[Coordinate] =
-    image
-      .zipWithIndex
+  def findGalaxies(image: Image): List[Coordinate] = image.zipWithIndex
       .flatMap { case (l, r) => l.zipWithIndex.map { case (el, c) => (el, (r, c)) }}
-      .filter(_._1 == '#')
-      .map(_._2)
+      .filter(_._1 == '#').map(_._2)
   def pairGalaxies(galaxies: List[Coordinate]): List[GalaxyPair] = {
     @tailrec
     def go(galaxies: List[Coordinate], pairs: List[GalaxyPair]): List[GalaxyPair] =
       if (galaxies.size < 2) pairs else go(galaxies.tail, pairs.appendedAll(galaxies.tail.map((_, galaxies.head))))
     go(galaxies, List.empty)
   }
-  def distanceBetween(pair: GalaxyPair): Long = Math.abs(pair._2._1 - pair._1._1) + Math.abs(pair._2._2 - pair._1._2)
   def findRowsToBeExpanded(image: Image): List[Int] = image.zipWithIndex.filter(_._1.forall(_ == '.')).map(_._2)
   def findColumnsToBeExpanded(image: Image): List[Int] = findRowsToBeExpanded(image.transpose)
   def distanceBetween(pair: GalaxyPair, expandedRows: List[Int], expandedCols: List[Int], expansionLevel: Int): Long = {
@@ -42,5 +38,4 @@ object Day11 {
 
   def task1(in: Seq[String]): Long = task(parseImage(in), 2)
   def task2(in: Seq[String]): Long = task(parseImage(in), 1000000)
-
 }
