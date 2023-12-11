79d32827437c607943665837a0c622ae47eec3ac
Balint Nagy <bnagy@cloudera.com>
2023-12-11 11:12:29 +0100

    Find build-in combinations method instead of manual pairing

--- scala/src/main/scala/puzzles/Day11.scala
+++ scala/src/main/scala/puzzles/Day11.scala
@@ -1,7 +1,5 @@
 package puzzles
 
-import scala.annotation.tailrec
-
 object Day11 {
 
   type Image = List[List[Char]]
@@ -12,12 +10,6 @@ object Day11 {
   def findGalaxies(image: Image): List[Coordinate] = image.zipWithIndex
       .flatMap { case (l, r) => l.zipWithIndex.map { case (el, c) => (el, (r, c)) }}
       .filter(_._1 == '#').map(_._2)
-  def pairGalaxies(galaxies: List[Coordinate]): List[GalaxyPair] = {
-    @tailrec
-    def go(galaxies: List[Coordinate], pairs: List[GalaxyPair]): List[GalaxyPair] =
-      if (galaxies.size < 2) pairs else go(galaxies.tail, pairs.appendedAll(galaxies.tail.map((_, galaxies.head))))
-    go(galaxies, List.empty)
-  }
   def findRowsToBeExpanded(image: Image): List[Int] = image.zipWithIndex.filter(_._1.forall(_ == '.')).map(_._2)
   def findColumnsToBeExpanded(image: Image): List[Int] = findRowsToBeExpanded(image.transpose)
   def distanceBetween(pair: GalaxyPair, expandedRows: List[Int], expandedCols: List[Int], expansionLevel: Int): Long = {
@@ -31,7 +23,7 @@ object Day11 {
   def task(image: Image, expansionLevel: Int): Long = {
     val expandedRows = findRowsToBeExpanded(image)
     val expandedCols = findColumnsToBeExpanded(image)
-    pairGalaxies(findGalaxies(image))
+    findGalaxies(image).combinations(2).map(c => (c.head, c.last))
       .map(pair => distanceBetween(pair, expandedRows, expandedCols, expansionLevel))
       .sum
   }
