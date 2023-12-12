9d353e24f0f5fc3343687fc3667ee9602e74a145
Balint Nagy <bnagy@cloudera.com>
2023-12-12 16:39:20 +0100

    Revert day 12 to using mutable map for better readability

--- scala/src/main/scala/puzzles/Day12.scala
+++ scala/src/main/scala/puzzles/Day12.scala
@@ -3,48 +3,40 @@ package puzzles
 object Day12 {
 
   def getValidOptions(springs: Seq[Char], damagedDist: Seq[Int], operationalToBeInserted: Int): Long = {
-    type Memo = Map[(Seq[Char], Seq[Int], Int), Long]
-
-    def go(springs: Seq[Char], damagedDist: Seq[Int], operationalToBeInserted: Int, memo: Memo): (Long, Memo) = {
-      def resultWithUpdatedMemo(h: Memo, v: Long): (Long, Memo) =
-        (v, h.updated((springs, damagedDist, operationalToBeInserted), v))
-
-      memo.get((springs, damagedDist, operationalToBeInserted)) match {
-        case Some(value) => (value, memo)
+    val memo = collection.mutable.Map.empty[(Seq[Char], Seq[Int], Int), Long]
+    def go(springs: Seq[Char], damagedDist: Seq[Int], operationalToBeInserted: Int): Long = {
+      val partialResult = memo.get((springs, damagedDist, operationalToBeInserted)) match {
+        case Some(value) => value
         case None =>
           springs.headOption match {
-            case None =>
-              resultWithUpdatedMemo(memo, if (damagedDist.isEmpty && operationalToBeInserted == 0) 1L else 0L)
-            case Some('.') => go(springs.tail, damagedDist, operationalToBeInserted, memo)
+            case None => if (damagedDist.isEmpty && operationalToBeInserted == 0) 1 else 0
+            case Some('.') => go(springs.tail, damagedDist, operationalToBeInserted)
             case Some('#') =>
               damagedDist.headOption match {
                 case Some(hasToBeDamaged) =>
-                  if (springs.take(hasToBeDamaged).contains('.')) {
-                    resultWithUpdatedMemo(memo, 0L)
-                  } else {
+                  if (springs.take(hasToBeDamaged).contains('.')) 0
+                  else
                     springs.drop(hasToBeDamaged) match {
-                      case '#' :: _ => resultWithUpdatedMemo(memo, 0L)
-                      case '.' :: remaining => go(remaining, damagedDist.tail, operationalToBeInserted, memo)
+                      case '#' :: _ => 0
+                      case '.' :: remaining => go(remaining, damagedDist.tail, operationalToBeInserted)
                       case '?' :: remaining =>
-                        if (operationalToBeInserted > 0)
-                          go(remaining, damagedDist.tail, operationalToBeInserted - 1, memo)
-                        else resultWithUpdatedMemo(memo, 0L)
-                      case Nil => go(Nil, damagedDist.tail, operationalToBeInserted, memo)
+                        if (operationalToBeInserted > 0) go(remaining, damagedDist.tail, operationalToBeInserted - 1)
+                        else 0
+                      case Nil => go(Nil, damagedDist.tail, operationalToBeInserted)
                     }
-                  }
-                case None => resultWithUpdatedMemo(memo, 0L)
+                case None => 0
               }
             case Some('?') =>
-              val (a, h) = go(springs.updated(0, '#'), damagedDist, operationalToBeInserted, memo)
-              val (b, h2) = if (operationalToBeInserted > 0)
-                go(springs.tail, damagedDist, operationalToBeInserted - 1, h)
-              else resultWithUpdatedMemo(h, 0L)
-              resultWithUpdatedMemo(h2, a + b)
+              val a = go(springs.updated(0, '#'), damagedDist, operationalToBeInserted)
+              val b = if (operationalToBeInserted > 0) go(springs.tail, damagedDist, operationalToBeInserted - 1) else 0
+              a + b
           }
       }
+      memo.update((springs, damagedDist, operationalToBeInserted), partialResult)
+      partialResult
     }
 
-    go(springs, damagedDist, operationalToBeInserted, Map.empty.asInstanceOf[Memo])._1
+    go(springs, damagedDist, operationalToBeInserted)
   }
 
   def task(dataLines: Seq[String], folds: Int): Long = dataLines.map(line => {
