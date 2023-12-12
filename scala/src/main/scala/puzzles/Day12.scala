74db8f97643c2ea8a3ef4b8149199199d2997676
Balint Nagy <bnagy@cloudera.com>
2023-12-12 15:35:50 +0100

    Refactor day 12 to replace mutable.Map with immutable.Map

--- scala/src/main/scala/puzzles/Day12.scala
+++ scala/src/main/scala/puzzles/Day12.scala
@@ -2,58 +2,66 @@ package puzzles
 
 object Day12 {
 
-  def getValidOptions(springs: Seq[Char], damagedDist: Seq[Int], dotsToBeInserted: Int): Long = {
-    val helper = collection.mutable.Map.empty[(Seq[Char], Seq[Int], Int), Long]
-    def go(springs: Seq[Char], damagedDist: Seq[Int], dotsToBeInserted: Int): Long = {
-      helper.get((springs, damagedDist, dotsToBeInserted)) match {
-        case Some(value) => value
+  def getValidOptions(springs: Seq[Char], damagedDist: Seq[Int], operationalToBeInserted: Int): Long = {
+    type Memo = Map[(Seq[Char], Seq[Int], Int), Long]
+
+    def go(springs: Seq[Char], damagedDist: Seq[Int], operationalToBeInserted: Int, memo: Memo): (Long, Memo) = {
+      def resultWithUpdatedMemo(h: Memo, v: Long): (Long, Memo) =
+        (v, h.updated((springs, damagedDist, operationalToBeInserted), v))
+
+      memo.get((springs, damagedDist, operationalToBeInserted)) match {
+        case Some(value) => (value, memo)
         case None =>
-          val result = springs.headOption match {
-            case None => if (damagedDist.isEmpty && dotsToBeInserted == 0) 1 else 0
-            case Some('.') => go(springs.tail, damagedDist, dotsToBeInserted)
+          springs.headOption match {
+            case None =>
+              resultWithUpdatedMemo(memo, if (damagedDist.isEmpty && operationalToBeInserted == 0) 1L else 0L)
+            case Some('.') => go(springs.tail, damagedDist, operationalToBeInserted, memo)
             case Some('#') =>
               damagedDist.headOption match {
-                case Some(damagedAmt) =>
-                  if (springs.take(damagedAmt).contains('.')) {
-                    0
+                case Some(hasToBeDamaged) =>
+                  if (springs.take(hasToBeDamaged).contains('.')) {
+                    resultWithUpdatedMemo(memo, 0L)
                   } else {
-                    val remaining = springs.drop(damagedAmt)
-                    remaining.headOption match {
-                      case Some('#') => 0
-                      case Some('.') => go(remaining.tail, damagedDist.tail, dotsToBeInserted)
-                      case Some('?') => if (dotsToBeInserted > 0) go(remaining.tail, damagedDist.tail, dotsToBeInserted - 1) else 0
-                      case None => go(remaining, damagedDist.tail, dotsToBeInserted)
+                    springs.drop(hasToBeDamaged) match {
+                      case '#' :: _ => resultWithUpdatedMemo(memo, 0L)
+                      case '.' :: remaining => go(remaining, damagedDist.tail, operationalToBeInserted, memo)
+                      case '?' :: remaining =>
+                        if (operationalToBeInserted > 0)
+                          go(remaining, damagedDist.tail, operationalToBeInserted - 1, memo)
+                        else resultWithUpdatedMemo(memo, 0L)
+                      case Nil => go(Nil, damagedDist.tail, operationalToBeInserted, memo)
                     }
                   }
-                case None => 0
+                case None => resultWithUpdatedMemo(memo, 0L)
               }
             case Some('?') =>
-              val a = go(springs.updated(0, '#'), damagedDist, dotsToBeInserted)
-              val b = if (dotsToBeInserted > 0) go(springs.tail, damagedDist, dotsToBeInserted - 1) else 0
-              a + b
+              val (a, h) = go(springs.updated(0, '#'), damagedDist, operationalToBeInserted, memo)
+              val (b, h2) = if (operationalToBeInserted > 0)
+                go(springs.tail, damagedDist, operationalToBeInserted - 1, h)
+              else resultWithUpdatedMemo(h, 0L)
+              resultWithUpdatedMemo(h2, a + b)
           }
-          helper.addOne((springs, damagedDist, dotsToBeInserted), result)
-          result
       }
     }
-    go(springs, damagedDist, dotsToBeInserted)
+
+    go(springs, damagedDist, operationalToBeInserted, Map.empty.asInstanceOf[Memo])._1
   }
 
-  def task(in: Seq[String], foldLevel: Int): Long = in.map(line => {
-      val Seq(rawStrings, rawDamagedDist) = line.split(" ").toSeq
-      val springs = (1 to foldLevel)
-        .flatMap(_ => rawStrings.toCharArray.appended('?'))
-        .dropRight(1).dropWhile(_ == '.').reverse.dropWhile(_ == '.').reverse
-      val cleanedSprings = springs
-        .sliding(2).flatMap { case Seq(a, b) => if (a == '.' && b == '.') Seq.empty else Seq(a) }
-        .toSeq.appended(springs.last)
-      val damaged = (1 to foldLevel).flatMap(_ => rawDamagedDist.split(",")).map(_.toInt)
-      val unknownIds = cleanedSprings.zipWithIndex.filter(_._1 == '?').map(_._2)
-      val damagedToBeInserted = damaged.sum - cleanedSprings.count(_ == '#')
-      val dotsToBeInserted = unknownIds.size - damagedToBeInserted
-      getValidOptions(cleanedSprings, damaged, dotsToBeInserted)
-    }).sum
+  def task(dataLines: Seq[String], folds: Int): Long = dataLines.map(line => {
+    val Seq(rawStrings, rawDamagedDist) = line.split(" ").toSeq
+    val unFoldedSprings = (1 to folds)
+      .flatMap(_ => rawStrings.toCharArray.appended('?'))
+      .dropRight(1).dropWhile(_ == '.').reverse.dropWhile(_ == '.').reverse
+    val cleanedSprings = unFoldedSprings
+      .sliding(2).flatMap { case Seq(a, b) => if (a == '.' && b == '.') Seq.empty else Seq(a) }
+      .toSeq.appended(unFoldedSprings.last)
+    val damaged = (1 to folds).flatMap(_ => rawDamagedDist.split(",")).map(_.toInt)
+    val unknown = cleanedSprings.count(_ == '?')
+    val damagedToBeInserted = damaged.sum - cleanedSprings.count(_ == '#')
+    val operationalToBeInserted = unknown - damagedToBeInserted
+    getValidOptions(cleanedSprings, damaged, operationalToBeInserted)
+  }).sum
 
-  def task1(in: Seq[String]): Long = task(in, 1)
-  def task2(in: Seq[String]): Long = task(in, 5)
+  def task1(dataLines: Seq[String]): Long = task(dataLines, 1)
+  def task2(dataLines: Seq[String]): Long = task(dataLines, 5)
 }
